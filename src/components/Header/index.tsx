1bc67483d0dd0d0c8fdcf8a60fb417484ab2d444
Leonardo Vargas <leu1607@hotmail.com>
2022-01-25 08:29:27 -0300

    styles: adicionando responsividade ao app

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { BsCart3 } from 'react-icons/bs'
 
 import styles from './styles.module.scss'
@@ -5,23 +6,38 @@ interface HeaderProps {
   cartQuant: number
 }
 
-export const Header = ({ cartQuant }: HeaderProps) => (
-  <header className={styles.container}>
-    <img src="/logo.png" alt="logo rocky's burguer" />
+export const Header = ({ cartQuant }: HeaderProps) => {
+  const [isVisibleMobileNav, setIsVisibleMobileNav] = useState(false)
 
-    <nav>
-      <ul className={styles.navigation}>
-        <li>HOME</li>
-        <li>BURGERS</li>
-        <li>SNACKS</li>
-        <li>DRINKS</li>
-        <li>CONTACTS</li>
-      </ul>
-    </nav>
+  return (
+    <header className={styles.container}>
+      <img src="/logo.png" alt="logo rocky's burguer" />
 
-    <button type="button" className={styles.cart}>
-      <BsCart3 size="2.5rem" />
-      {!!cartQuant && <span>{cartQuant}</span>}
-    </button>
-  </header>
-)
+      <nav className={isVisibleMobileNav ? styles.active : ''}>
+        <ul className={styles.navigation}>
+          <li>HOME</li>
+          <li>BURGERS</li>
+          <li>SNACKS</li>
+          <li>DRINKS</li>
+          <li>CONTACTS</li>
+        </ul>
+      </nav>
+
+      <button type="button" className={styles.cart}>
+        <BsCart3 size="2.5rem" />
+        {!!cartQuant && <span>{cartQuant}</span>}
+      </button>
+
+      <button
+        type="button"
+        className={`${styles['toggle-mobile']} ${
+          isVisibleMobileNav ? styles['toggle-active'] : ''
+        }`}
+        onClick={() => setIsVisibleMobileNav(state => !state)}
+      >
+        <span />
+        <span />
+      </button>
+    </header>
+  )
+}
